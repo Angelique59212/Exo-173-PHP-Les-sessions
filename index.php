46ec09aefc11a53c0f3e7a279f943202ac834349
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-31 09:50:28 +0100

    fin

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start()?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,22 +10,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body style="background: <?= $_SESSION['color']?>">
 
     <h1>Choix de couleur:</h1>
 
     <form action="script.php" method="post">
         <label for="choose-color">Choisis une couleur:</label>
-        <select name="color" id="choose-color">
-            <option value="color-red">Rouge</option>
-            <option value="color-blue">Blue</option>
-            <option value="color-pink">Rose</option>
-            <option value="color-cyan">Cyan</option>
-            <option value="color-black">Black</option>
+        <select name="choose-color" id="choose-color">
+            <option value="red">Rouge</option>
+            <option value="blue">Blue</option>
+            <option value="pink">Rose</option>
+            <option value="cyan">Cyan</option>
+            <option value="black">Black</option>
         </select>
 
-        <input type="submit" value="Valider" id="submit">
+        <input type="submit" value="Valider" name="submit">
     </form>
+
+    <ul>
+        <li><a href="page1.php">Page1</a></li>
+        <li><a href="page2.php">Page2</a></li>
+    </ul>
+
 
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,4 +1,9 @@
 <?php
+
 session_start();
 
+if (isset($_POST['submit'])) {
+    $_SESSION['color'] = $_POST['choose-color'];
+    header('Location: index.php');
+}
 
